b7110e4bb4009d461a9d2fe4b0b72f25fba2fd29
kyranmccann <kyran.mccann@gmail.com>
2019-02-18 09:12:00 -0800

    linear search

--- project/searching.py
+++ project/searching.py
@@ -1,17 +1,19 @@
-# STRETCH: implement Linear Search				
+# STRETCH: implement Linear Search
 def linear_search(arr, target):
-  
+
   # TO-DO: add missing code
+    for i in range(0, len(arr)):
+        if arr[i] == target:
+            return i
+    return -1   # not found
 
-   return -1   # not found
 
-
-# STRETCH: write an iterative implementation of Binary Search 
+# STRETCH: write an iterative implementation of Binary Search
 def binary_search(arr, target):
 
   if len(arr) == 0:
     return -1 # array empty
-    
+
   low = 0
   high = len(arr)-1
 
@@ -20,9 +22,9 @@ def binary_search(arr, target):
   return -1 # not found
 
 
-# STRETCH: write a recursive implementation of Binary Search 
+# STRETCH: write a recursive implementation of Binary Search
 def binary_search_recursive(arr, target, low, high):
-  
+
   middle = (low+high)/2
 
   if len(arr) == 0:
