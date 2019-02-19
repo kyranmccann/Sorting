9266b076623f366cb888b5d66169c2647ac8a352
kyranmccann <kyran.mccann@gmail.com>
2019-02-19 14:42:32 -0800

    quicksort with first element as pivot

--- project/recursive_sorting.py
+++ project/recursive_sorting.py
@@ -1,11 +1,11 @@
-### helper function
-def merge( arrA, arrB ):
-    elements = len( arrA ) + len( arrB )
+# helper function
+def merge(arrA, arrB):
+    elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
     a = 0
     b = 0
     # since arrA and arrB already sorted, we only need to compare the first element of each when merging!
-    for i in range( 0, elements ):
+    for i in range(0, elements):
         if a >= len(arrA):    # all elements in arrA have been merged
             merged_arr[i] = arrB[b]
             b += 1
@@ -21,12 +21,12 @@ def merge( arrA, arrB ):
     return merged_arr
 
 
-### recursive sorting function
-def merge_sort( arr ):
-    if len( arr ) > 1:
-        left = merge_sort( arr[ 0 : len( arr ) / 2 ] )
-        right = merge_sort( arr[ len( arr ) / 2 : ] )
-        arr = merge( left, right )   # merge() defined later
+# recursive sorting function
+def merge_sort(arr):
+    if len(arr) > 1:
+        left = merge_sort(arr[0:int(len(arr) / 2)])
+        right = merge_sort(arr[int(len(arr) / 2):])
+        arr = merge(left, right)   # merge() defined later
     return arr
 
 
@@ -36,14 +36,28 @@ def merge_in_place(arr, start, mid, end):
 
     return arr
 
-def merge_sort_in_place(arr, l, r): 
+
+def merge_sort_in_place(arr, l, r):
     # TO-DO
 
     return arr
 
 
 # TO-DO: implement the Quick Sort function below USING RECURSION
-def quick_sort( arr, low, high ):
+def quick_sort(arr, low, high):
+    if high-low <= 0:
+        return arr
+    # pivot at the first element
+    pivot = low
+    for i in range(low+1, high+1):
+        if arr[i] < arr[pivot]:
+            arr[i], arr[pivot+1] = arr[pivot+1], arr[i]
+
+            arr[pivot], arr[pivot+1] = arr[pivot+1], arr[pivot]
+            pivot += 1
+
+    arr = quick_sort(arr, low, pivot-1)
+    arr = quick_sort(arr, pivot+1, high)
 
     return arr
 
